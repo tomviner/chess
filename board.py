936ec41882356a3e5b4f8edf3299f887ec05b44e
TomV <tom@viner.tv>
2014-04-19 23:37:13 +0100

    messy pass on test_move_piece

--- board.py
+++ board.py
@@ -1,3 +1,4 @@
+EMPTY_SQUARE = '.'
 
 class Board(object):
     def __init__(self, height, width):
@@ -5,9 +6,7 @@ class Board(object):
         self.width = width
         self._board = []
         for y in xrange(height):
-            row = []
-            for x in xrange(width):
-                row.append('.')
+            row = [EMPTY_SQUARE] * width
             self._board.append(row)
 
     def display(self):
@@ -21,3 +20,8 @@ class Board(object):
             y - the rank, starting from 0, bottom up
         """
         self._board[self.height-y-1][x] = piece
+
+    def move(self, x1, y1, x2, y2):
+        piece = self._board[self.height-y1-1][x1]
+        self._board[self.height-y1-1][x1] = EMPTY_SQUARE
+        self._board[self.height-y2-1][x2] = piece
--- test_chess.py
+++ test_chess.py
@@ -16,23 +16,35 @@ def test_board():
 def test_place_piece():
     b = Board(3, 3)
     b.place(1, 2, 'X')
-    exed_board = dedent(
+    expected_board = dedent(
     """
     .X.
     ...
     ...
     """).strip()
-    assert b.display() == exed_board
+    assert b.display() == expected_board
 
 def test_place_more_pieces():
     b = Board(2, 2)
-    b.place(0, 1, 'a')
-    b.place(1, 1, 'b')
-    b.place(0, 0, 'c')
-    b.place(1, 0, 'd')
-    exed_board = dedent(
+    grid = {
+        (0, 1): 'a', (1, 1): 'b',
+        (0, 0): 'c', (1, 0): 'd'}
+    for (x, y), piece in grid.items():
+        b.place(x, y, piece)
+    expected_board = dedent(
     """
     ab
     cd
     """).strip()
-    assert b.display() == exed_board
+    assert b.display() == expected_board
+
+def test_move_piece():
+    b = Board(2, 2)
+    b.place(1, 1, 'X')
+    b.move(1, 1, 0, 0)
+    expected_board = dedent(
+    """
+    ..
+    X.
+    """).strip()
+    assert b.display() == expected_board
