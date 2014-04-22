36d9b235534f3ca23d2fbf2607da23835638091b
TomV <tom@viner.tv>
2014-04-22 19:31:36 +0100

    set empty_square_char on Board class

--- board.py
+++ board.py
@@ -1,15 +1,16 @@
 from exception import BadMove
 
 
-EMPTY_SQUARE = '.'
 
 class Board(object):
+    empty_square_char = '.'
+
     def __init__(self, height, width):
         self.height = height
         self.width = width
         self._board = []
         for y in xrange(height):
-            row = [EMPTY_SQUARE] * width
+            row = [self.empty_square_char] * width
             self._board.append(row)
 
     def _get(self, x, y):
@@ -35,7 +36,7 @@ class Board(object):
         Move the piece at x1, xy to x2, y2
         """
         piece = self._get(x1, y1)
-        if piece == EMPTY_SQUARE:
+        if piece == self.empty_square_char:
             raise BadMove
-        self._set(x1, y1, EMPTY_SQUARE)
+        self._set(x1, y1, self.empty_square_char)
         self._set(x2, y2, piece)
