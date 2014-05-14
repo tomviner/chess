e39418c4352d3bc4f42d072020b3c558099a0f06
TomV <tom@viner.tv>
2014-05-14 19:34:02 +0100

    passing notation tests, need refactor

--- chess/game.py
+++ chess/game.py
@@ -1,3 +1,5 @@
+import string
+
 from .board import Board
 from .exception import InputError
 
@@ -10,6 +12,10 @@ class Game(object):
     """
     def __init__(self, initial):
         self._board = Board.from_string(initial)
+        width = self._board.width
+        self.column_letters = string.letters[:width].lower()
+        height = self._board.height
+        self.row_digits = string.digits[1:height+1]
 
     def board_display(self):
         return self._board.display()
@@ -20,13 +26,26 @@ class Game(object):
         and return parsed to coords
         """
         s = raw_input('Move: ')
-        from_, to = s.strip().lower().split()
-        def parse_move(coord_string):
-            import string
-            print coord_string
-            column_letter, row_number = coord_string
-            x = string.letters.index(column_letter)
-            y = string.digits[1:].index(row_number)
-            return x, y
-        return parse_move(from_), parse_move(to)
+        cleaned_input = s.strip().lower()
 
+        try:
+            from_, to = cleaned_input.split()
+        except ValueError:
+            raise InputError
+
+        return self.parse_square(from_), self.parse_square(to)
+
+    def parse_square(self, coord_string):
+        try:
+            column_letter, row_number = coord_string.strip()
+        except ValueError:
+            raise InputError("Couldn't find a label from {!r}".format(coord_string))
+        try:
+            x = self.column_letters.index(column_letter)
+        except ValueError:
+            raise InputError("Couldn't find a column from {!r} in {!r}".format(column_letter, self.column_letters))
+        try:
+            y = self.row_digits.index(row_number)
+        except ValueError:
+            raise InputError("Couldn't find a row from {!r} in {!r}".format(row_number, self.row_digits))
+        return x, y
--- chess/test_game.py
+++ chess/test_game.py
@@ -3,11 +3,27 @@ from textwrap import dedent
 import pytest
 from mock import patch
 
-from .game import Game
 from .exception import InputError
+from .game import Game
 
+
+CHESS = dedent(
+    """
+    rnbkqbnr
+    pppppppp
+    ........
+    ........
+    ........
+    ........
+    PPPPPPPP
+    RNBQKBNR
+    """).strip()
 
 def test_game_initialisation():
+    """
+    Check the board created from a string looks
+    the same when re-displayed
+    """
     DRAUGHTS = dedent(
     """
     .O.O.O.O
@@ -22,18 +38,6 @@ def test_game_initialisation():
     X.X.X.X.
     """).strip()
 
-    CHESS = dedent(
-    """
-    rnbkqbnr
-    pppppppp
-    ........
-    ........
-    ........
-    ........
-    PPPPPPPP
-    RNBQKBNR
-    """).strip()
-
     for game_layout in (DRAUGHTS, CHESS):
         game = Game(initial=game_layout)
         assert game.board_display() == game_layout
@@ -41,24 +45,39 @@ def test_game_initialisation():
 @patch('chess.game.raw_input', create=True)
 def test_taking_input(mock_raw_input):
     mock_raw_input.return_value = 'b2 e4'
-    assert Game(initial='.').get_move() == ((1, 1), (4, 3))
+    assert Game(initial=CHESS).get_move() == ((1, 1), (4, 3))
 
-
-@pytest.mark.parametrize("input", (
+@pytest.mark.parametrize("input_move", (
     "notcorrect",
     "not correct",
     "so not correct",
     # only first 8 letters, a-h, not i-z
     "i1 a1",
     "d1 z2",
-    # only first 8 digits (1 indexed), not 0
+    # only first 8 digits (1 indexed), not 0 or 9+
     "a0 b2",
-    # only first 8 digits (1 indexed), not 9 onwards
     "c7 a9",
     "d8 a10",
 ))
 @patch('chess.game.raw_input', create=True)
 def test_bad_input(mock_raw_input, input_move):
+    """
+    Check some invalid edge cases are indeed rejected
+    """
+    mock_raw_input.return_value = input_move
     with pytest.raises(InputError):
-        mock_raw_input.return_value = input_move
-        Game(initial=' ').get_move()
+        Game(initial=CHESS).get_move()
+
+@pytest.mark.parametrize("input_move", (
+    "b2 b4",
+    "a1 b1",
+    "a1 a2",
+))
+@patch('chess.game.raw_input', create=True)
+def test_size_dependant(mock_raw_input, input_move):
+    full_size_board = CHESS
+    dummy_tiny_board = '.'
+    mock_raw_input.return_value = input_move
+    with pytest.raises(InputError):
+        Game(initial=dummy_tiny_board).get_move()
+    Game(initial=full_size_board).get_move()
